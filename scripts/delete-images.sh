facce91565a4c4534319a682621e3ce5fde1fe1f
Adam Rocha <40733364+adamrocha@users.noreply.github.com>
2026-01-14 16:38:56 -0800

    Revert "Add Makefile and update README for CloudX EKS deployment"

--- scripts/delete-images.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-aws ecr list-images --repository-name cloudx-app-repo --query 'imageIds[*]' --output json | aws ecr batch-delete-image --repository-name cloudx-app-repo --image-ids file:///dev/stdin
